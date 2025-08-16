ba9fe59713ce88a0d3ce8478620bcecf23c6ae34
Yohann Caboni <yohanncaboni@macbookpro-1.home>
2025-08-16 14:20:20 +0200

    build main screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,39 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
+export default function NotFound() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
+    <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+    <View style={styles.container}>
+      <Text style={styles.text}>This screen doesn't exist.</Text>
+      <Link href="/" style={styles.button}>Go to Home screen</Link>
+    </View>
+    </> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  text: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  button: {
+    color: '#333333',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 20,
+    padding: 16,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    textDecorationLine: 'none',
+    textAlign: 'center',
   },
 });
